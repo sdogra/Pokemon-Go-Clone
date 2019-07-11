cb4c33c6ab1380f1ce5347609f1a755d75117840
dogras <dograsahil00@gmail.com>
2019-07-11 17:47:18 -0400

    Finished Center button and trainer icon

--- Pokemon Go Clone/MapViewController.swift
+++ Pokemon Go Clone/MapViewController.swift
@@ -38,7 +38,38 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         mapView.delegate = self
     }
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let annoView = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        if (annotation is MKUserLocation){
+            annoView.image = UIImage(named: "player")
+            var frame = annoView.frame
+            frame.size.height = 50.0
+            frame.size.width = 50.0
+            annoView.frame = frame
+        }
+        return annoView
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if (updateCount < 3) {
+            if let center = manager.location?.coordinate {
+                let region = MKCoordinateRegion(center: center, latitudinalMeters: 200, longitudinalMeters: 200)
+                mapView.setRegion(region, animated: false)
+            }
+            updateCount += 1
+        }
+        else {
+            manager.stopUpdatingLocation()
+        }
+        
+        
+    }
+    
     @IBAction func centerTapped(_ sender: Any) {
+        if let center = manager.location?.coordinate {
+            let region = MKCoordinateRegion(center: center, latitudinalMeters: 200, longitudinalMeters: 200)
+            mapView.setRegion(region, animated: true)
+        }
     }
     
 }
